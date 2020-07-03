18149931d2a48e881029677a28003a0ac58b6674
KZTN <kakaiocesar2@gmail.com>
2020-07-03 18:07:58 -0300

    :art: Added Provider to main tsx
    all changes are functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Router from "./routes";
+import { Provider } from "react-redux";
+import store from "./store";
 
 import "./styles/global.scss";
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Router />
+      <Provider store={store}>
+        <Router />
+      </Provider>
     </div>
   );
 };
